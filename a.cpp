2917bcaa1376844d99710caa17a61562187dc7ba
a9irudhh <01fe22bcs264@kletech.ac.in>
2024-12-09 20:26:32 +0530

    Testing

---
+++ a.cpp
@@ -0,0 +1,41 @@
+// Written by Anirudh R. H.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////////////////////////////////////--definitions and functionalities--//////////////////////////////////////////////////////////////
+
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf 1e18
+#define MOD 1e9+7
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+/*
+felt cute, might delete it later :3
+*/
+
+void printRec(lli n){
+    if(n == 0) return;
+    printRec(n-1);
+    cout << "I love Recursion" << endl;
+    return;
+}
+void solutionForProblem() {
+    lli n; cin >> n;
+    printRec(n-1);
+}
+
+////////////////////////////////////////////--Main Function--/////////////////////////////////////////////////////////////
+
+int main() {
+    NFS;
+    lli testCases = 1;
+    // cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
