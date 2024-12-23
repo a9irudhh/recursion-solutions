6119c2d439fc7b08fe53de5384dc0631a2cbc7d5
a9irudhh <01fe22bcs264@kletech.ac.in>
2024-12-24 01:48:33 +0530

    recursion done

--- Recursion/m.cpp
+++ Recursion/m.cpp
@@ -1,1 +1,48 @@
-// m.cpp
+// Written by Anirudh R. H.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////////////////////////////////////--definitions and functionalities--//////////////////////////////////////////////////////////////
+
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf 1e18
+#define MOD 1e9+7
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+/*
+felt cute, might delete it later :3
+*/
+
+lli getSum(vector<lli>& v, lli i, lli n) {
+    if (i == n - 1) return v[i];
+    v[i] = v[i] + getSum(v, i + 1, n);
+    return v[i];
+}
+
+void solutionForProblem() {
+    lli n, m;
+    cin >> n >> m;
+    vector<lli> v(n);
+    f(i, 0, n) cin >> v[i];
+    
+    getSum(v, 0, n);
+    // for(auto it: v) cout << it << ' ';
+    //     cout << endl;
+    cout << v[n - m] << endl;
+}
+
+////////////////////////////////////////////--Main Function--/////////////////////////////////////////////////////////////
+
+int main() {
+    NFS;
+    lli testCases = 1;
+    // cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
--- Recursion/x.cpp
+++ Recursion/x.cpp
@@ -1,1 +1,47 @@
-// x.cpp
+// Written by Anirudh R. H.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////////////////////////////////////--definitions and functionalities--//////////////////////////////////////////////////////////////
+
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf LLONG_MAX
+#define MOD 1e9+7
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+/*
+felt cute, might delete it later :3
+*/
+lli help(vector<vector<lli>> &v, lli x, lli y, lli n, lli m) {
+    if(x >= n || y >= m) return -inf;
+    else if(x == n-1 && y == m-1) return v[x][y];
+
+    lli one = help(v, x+1, y, n, m);
+    lli two = help(v, x, y+1, n, m);
+
+    return v[x][y] + max(one, two);
+}
+
+void solutionForProblem() {
+    lli n, m; cin >> n >> m;
+    vector<vector<lli>> v(n, vector<lli> (m));
+
+    f(i, 0, n) f(j, 0, m) cin >> v[i][j];
+    cout << help(v, 0, 0, n, m) << endl;
+}
+
+////////////////////////////////////////////--Main Function--/////////////////////////////////////////////////////////////
+
+int main() {
+    NFS;
+    lli testCases = 1;
+    // cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
